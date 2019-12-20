795a1ac59c3cb7763b27e56adf3e844946f4e330
wangxu <wangxu@19pay.com.cn>
2019-12-20 15:38:03 +0800

    delete kafka and canal client

--- canal/canal_client/refresh_kerberos.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-#@author: wangxiao
-#@date: 2018-08-23
-
-kinit -kt bussiness.keytab car_owner/carowner
--- canal/canal_client/src/com/switchfield/base/SwitcherStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.switchfield.base;
-
-/**
- * Created by WangXiao on 2019/4/28.
- * 字段值转换方法基类
- */
-public abstract class SwitcherStrategy {
-    public abstract String switchValue(String oldValue, String unknownParam);
-}
